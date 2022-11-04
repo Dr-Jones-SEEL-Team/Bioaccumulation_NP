84d17e86710d2539b489fdffaf72a60d38f8e042
jprince58 <65364454+jprince58@users.noreply.github.com>
2022-11-04 08:33:24 -0400

    end of day Nov 3
    Almost have model vs literature results lined up. Weird problems with time=14 and 28 minutes, but otherwise I think the script is almost ready. Two things to add to script: ensuring x=1 is biofilm surface (currently it is about x=0.9 because of nature of literature data), and need to plot mode vs literature values. The other thing that needs to be done (not in this script) is how to make my boundary condition a function of time. If I just make the boundary condition a function of time in the code, will that have other downstream effects? Will need to just sit down and ponder that one before making the change.

--- N3/N3_exp_data_fitter.py
+++ N3/N3_exp_data_fitter.py
@@ -23,9 +23,23 @@ def exp_data_fitter(c_set,exp_data,parameter_combos_count):
         x=c_set[pc_i][9] #Grab the position-vector for this parameter set for plotting
         t=c_set[pc_i][7] #Grab time-vector for this parameter set for plotting
         
-        #Get model results into array similar to experimental results
+        # %% Convert dimensionless mdoel results inot dimenional model results
+        to= 5 #guess at dimensionless time [min]
+        kconv=200 #guess at absorbance units-NP concentration conversion factor
+        t_d=t*to #convert dimensionless time into dimensional time [min]
+        ct_d=kconv*ct #convert dimensionless total concentration to absorbance units
         
-        
+        # %% Get model results into array similar to experimental results
+        mod_data=np.zeros((len(t)*len(x),3)) #Initialize array
+        for xi in np.arange(0,len(x)):
+            position=x[xi]
+            for ti in np.arange(0,len(t_d)):
+                time=t_d[ti]
+                index=xi+len(x)*ti #sent index in the mod_data matrix
+                mod_data[index,0]=time
+                mod_data[index,1]=position
+                mod_data[index,2]=ct_d[xi,ti]
+            
         # %% Remove baseline from experimental AU values
         baseline=8.20 #This is apprently the baseline AU value, when the experimental results should really be zero (eye-balled it)
         exp_data[:,2]=exp_data[:,2]-baseline
@@ -34,29 +48,23 @@ def exp_data_fitter(c_set,exp_data,parameter_combos_count):
         exp_data_shape=np.shape(exp_data) #Shape of experimental data
         collated_results=np.append(exp_data,np.zeros((exp_data_shape[0],1)),axis=1) #Initialize vector which ahs expeirmental and model results. First three columns ar eexperimental data, last column is model fit
         
-        # %% Convert dimensionless mdoel results inot dimenional model results
-        to= 3 #guess at dimensionless time [min]
-        kconv=200 #guess at absorbance units-NP concentration conversion factor
-        t_d=t*to #convert dimensionless time into dimensional time [min]
-        ct_d=kconv*ct #convert dimensionless total concentration to absorbance units
-        
         # %% Calculate model values to put in collated vector
 
-         #Need to interpolate between model values to get the neccesary experimental predictions
-        # ti_old=-1
-        # for i in np.arange(0,len(collated_results)): #loop over all expeirmental values
-        #     ti_new=collated_results[i,0] #grab time-point to use
-        #     if ti_old==ti_new:
-        #         pass
-        #     else: #when we have a new time to extract
-        #         ti_old=ti_new
-        #         t_subset=collated_results[collated_results[:, 0] == ti_old, :] #grab only experimental values at that time-point
-                
-                
-                
-        #     x_i=collated_results[i,2]
-        #     for j in np.arange(0,len(x)):
-        #         if x>i
+        ti_old=-1
+        for i in np.arange(0,len(collated_results)): #loop over all expeirmental values
+            ti_new=collated_results[i,0] #grab time-point to use
+            if ti_old==ti_new:
+                pass
+            else: #when we have a new time to extract
+                ti_old=ti_new
+                t_exp_subset=collated_results[collated_results[:, 0] == ti_old, :] #grab only experimental values at that time-point
+                t_mod_subset=mod_data[mod_data[:,0]==int(ti_old), :] #grab model values at this timepoint
+                for j in np.arange(0,len(t_exp_subset)):
+                    x_inter=t_exp_subset[j,1] #position value which needs its corresponding model value interpolated
+                    for k in np.arange(0,len(t_mod_subset)):
+                        if x_inter>t_mod_subset[k,1] and x_inter<t_mod_subset[k+1,1]: #check if x-value is greater current model-x and less than next (where to interpolate)
+                            collated_results[i+j,3] = t_mod_subset[k,2]+(t_mod_subset[k+1,2]-t_mod_subset[k,2])*(x_inter-t_mod_subset[k,1])/(t_mod_subset[k+1,1]-t_mod_subset[k,1]) #standard interpolation formula
+                        else: pass
         
     return 0
 
